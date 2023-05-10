4d270b9de230e6b5248c62bc2677c772fa58c9f4
coding-gen <coding.gen@gmail.com>
2023-05-09 23:58:06 -0600

    vehicle and bus

---
+++ pynative/pynative_oop.py
@@ -0,0 +1,44 @@
+#! /usr/bin/python3
+
+"""
+Author: Genevieve LaLonde
+
+Python Object Oriented Programming coding exercise from: 
+https://pynative.com/python-object-oriented-programming-oop-exercise
+"""
+
+class Vehicle:
+	# Vehicle class without any variables nor methods
+	pass
+
+
+class Vehicle:
+	# create a Vehicle class with max_speed and mileage instance attributes.
+	def __init__(self, name, max_speed=120, mileage=0, capacity=5):
+		self.name = name
+		self.max_speed = max_speed
+		self.mileage = mileage
+		self.capacity = capacity
+
+
+	def __str__(self):
+		return f"name: {self.name}\nspeed: {self.max_speed}\nmileage: {self.mileage}\nseating: {self.capacity}"
+
+
+class Bus(Vehicle):
+	# Bus object inherits the parent Vehicle class.
+	def __init__(self, name, max_speed=80, mileage=2000, capacity=50):
+		super(Bus, self).__init__(name, max_speed, mileage, capacity)
+
+
+
+def main():
+	modelX = Vehicle('modelX', 240, 18)
+	print(f"vehicle modelX: \n{modelX}")
+	school_bus = Bus("School Volvo", 180, 12)
+	print(f"\nvehicle school bus: \n{school_bus}")
+
+
+if __name__ == '__main__':
+	main()
+
